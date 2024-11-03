f1b2dfd06e0301424cdc5d3dea5b5222bf426a15
brilliahib <brilliahib21@gmail.com>
2024-11-04 00:07:51 +0700

    feat: logout functionallity

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import HeaderTitle from "../components/atoms/typography/HeaderTitle";
 import { useAuth } from "../context/AuthContext";
+import { generateFallbackFromName } from "../utils/misc";
+import { LogOut } from "lucide-react";
 
 export default function AccountPage() {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
 
   const handleLogout = () => {
     logout();
@@ -16,10 +18,24 @@ export default function AccountPage() {
           title="Account"
           subtitle="Access your account details in here"
         />
+        <div className="space-y-4">
+          <div className="flex items-center gap-4">
+            <div className="relative flex h-24 w-24 shrink-0 overflow-hidden rounded-full">
+              <div className="flex h-full w-full items-center justify-center rounded-full bg-[#f5f5f5] text-gray-700 md:text-2xl font-bold">
+                {generateFallbackFromName(user.fullname)}
+              </div>
+            </div>
+            <div className="space-y-1">
+              <h1 className="font-semibold">{user.fullname}</h1>
+              <p className="text-[#737373]">{user.email}</p>
+            </div>
+          </div>
+        </div>
         <button
           onClick={handleLogout}
-          className="mt-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-500 transition"
+          className="mt-4 flex gap-2 items-center px-6 py-2 text-red-500 bg-white border border-red-500 hover:bg-red-500 hover:text-white text-white rounded-md transition text-sm"
         >
+          <LogOut className="h-4 w-4" />
           Logout
         </button>
       </div>
